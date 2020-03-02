876a1b7cc348c6e02474f82b6aee7b3ac9849614
Iremiwa <somoayo@yahoo.com>
2020-03-02 07:12:19 +0100

    Updates

---
+++ precache-manifest.c35897d4e2ac5dd6e7938f9275732b6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa4e99d368d470198405dd651bc896",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "c049af43be69c0339c54",
+    "url": "/my-portfolio/static/css/main.edd71ea3.chunk.css"
+  },
+  {
+    "revision": "2e034af13bb0623c5dd7",
+    "url": "/my-portfolio/static/js/2.f72d5276.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-portfolio/static/js/2.f72d5276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c049af43be69c0339c54",
+    "url": "/my-portfolio/static/js/main.fdb09619.chunk.js"
+  },
+  {
+    "revision": "7f65ad689fbac161c084",
+    "url": "/my-portfolio/static/js/runtime-main.3734a9be.js"
+  },
+  {
+    "revision": "e2b0fd204f3ba482ffc2ad454186237e",
+    "url": "/my-portfolio/static/media/bookFinderMockup.e2b0fd20.png"
+  },
+  {
+    "revision": "e00c8b0760f282d5060b51e462cacf80",
+    "url": "/my-portfolio/static/media/gifFinderMockup.e00c8b07.png"
+  },
+  {
+    "revision": "39661ae180d0c3ee5f1609855ea46cf3",
+    "url": "/my-portfolio/static/media/header2.39661ae1.jpg"
+  },
+  {
+    "revision": "2695439c94fb8244ec03c8026d28f4d1",
+    "url": "/my-portfolio/static/media/instabillMockup.2695439c.png"
+  },
+  {
+    "revision": "7671d088506940ac47e1d422d84d239e",
+    "url": "/my-portfolio/static/media/newsSearchMockup.7671d088.png"
+  },
+  {
+    "revision": "c5736b9ad077291b7f1eef035d4b6d56",
+    "url": "/my-portfolio/static/media/quizAppMockup.c5736b9a.png"
+  },
+  {
+    "revision": "71138fa13779dff0fb0fc365cd856739",
+    "url": "/my-portfolio/static/media/todoListMockup.71138fa1.png"
+  }
+]);
